2a89a4fc33ce8d10563efb6129895040c905e314
luisfeCL <92877836+luisfeCL@users.noreply.github.com>
2022-06-08 22:13:07 +0200

    esoty desesperado por favor ayuda

--- src/pages/project/project.jsx
+++ src/pages/project/project.jsx
@@ -1,8 +1,6 @@
 import { useParams} from 'react-router-dom'
 import './project.css'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+
 
 
 
@@ -11,16 +9,7 @@ export default function Project(){
 
     const params = useParams();
 
-    const [projects, setProjects]= useState([]);
-
-    useEffect(() => {
-        axios.get('/projects.json')
-        .then(response => setProjects(response.data.works));
-    }, [])
-
     return <h1 className='projectTitle'> 
-            {projects.map(project => <Link to={`/project/${project.urlname}`}>
-                                <p> {project.year} {project.name} {project.category}</p>
-                            </Link>)}
+            {params.name}
         </h1>;   
 }
